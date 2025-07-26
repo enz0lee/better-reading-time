6728468c8c7a33d0b40df7770c947d32922139f3
Enzo Lee <soloenzo.dev@gmail.com>
2025-07-26 18:55:03 +0700

    feat: add tailwind

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,11 +1,11 @@
+import { plugins } from './node_modules/@wxt-dev/browser/src/gen/index.d'
 import { defineConfig } from 'wxt'
+import tailwindcss from '@tailwindcss/vite'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
-  imports: {
-    eslintrc: {
-      enabled: 9,
-    },
-  },
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
 })
